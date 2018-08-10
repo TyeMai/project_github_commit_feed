dfdc9801a6a63ea01097ce0854bca5b0b4816018
TyeMai <tyeers@gmail.com>
2018-08-10 05:50:52 -0700

    update to readme to test webhooks

--- services/getCommitData.js
+++ services/getCommitData.js
@@ -36,7 +36,7 @@ loadPages.form = (req, res) => {
       "Content-Type": "text/html"
     })
     var p = new Promise((resolve) => {
-      console.log(path.href)
+      //console.log(path.href)
       info.userNameMatch = path.href.match(/username=([^&]+)/)[1]
       info.repoMatch = path.href.match(/repo=([a-zA-Z_-]+)/)[1]
       //console.log(info.userNameMatch)
@@ -46,41 +46,33 @@ loadPages.form = (req, res) => {
       if (info.userNameMatch === null) {
         return
       }
-      //console.log(info)
       resolve(info)
     }).then((info) => {
       var results = github_request.getCommits(info)
-      //console.log(results)
       return results
     }).then((results) => {
-      //console.log("these are the results" + results)
-      //c;lkjonsole.log(results)
-      /*
-      console.log('im the results')
-      console.log(results.data[0].commit.author)
-      console.log(results.data[0].commit.message)
-      console.log(results.data[0].commit.url)
-      console.log(results.data[0].sha)
-*/
-      var commits = []
+      var scrubCommits = []
       var someFuckingName = {}
-      for(var commit of results.data){
-
-        commits.push(someFuckingName = {
-          author: commit.commit.author ,
-          message: commit.commit.message ,
-          sha:   commit.sha ,
+      for (var commit of results.data) {
+        scrubCommits.push(someFuckingName = {
+          author: commit.commit.author,
+          message: commit.commit.message,
+          sha: commit.sha,
           url: commit.commit.url
         })
-
-      //  console.log(commit.sha)
-      //  console.log(commit.commit.author)
-      //  console.log(commit.commit.message)
-      //  console.log(commit.commit.url)
       }
+      //scrubCommits = JSON.stringify(scrubCommits, null, 2)
+      //fs.writeFile('./data/commits.json', scrubCommits, 'utf8', (err)=>{
+      //  if(err) throw err;
+      //})
+      //  console.log(commits)
+        //var htmlOut = data.replace("{{ commitFeed }}", JSON.stringify(scrubCommits, null, 2))
+        var htmlOut = data.replace("{{ commitFeed }}", commits)
 
-      var htmlOut = data.replace("{{ commitFeed }}", JSON.stringify(commits, null, 2))//JSON.stringify(results, null, 2))
-      res.end(htmlOut)
+        res.end(htmlOut)
+
+
+
     })
   })
 }
