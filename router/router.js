e3d8f1e45180efaee8028d4a55a002fb81211933
TyeMai <tyeers@gmail.com>
2018-08-13 04:11:20 -0700

    made variable names better

--- router/router.js
+++ router/router.js
@@ -15,67 +15,25 @@ Router.routes = {}
 
 //forOf loops have strange behavior in this case.
 Router.methods.forEach(method => {
-
   Router.routes[method] = Router.routes[method] || {}
-
   Router[method] = (path, callback) => {
-    //  console.log(Router +  " before")
-    //console.log(Router)
     Router.routes[method][path] = callback;
-    //  console.log(Router + " after")
   }
 })
 
-
-/*
-for (var method of Router.methods) {
-  Router.routes[method] = Router.routes[method] || {}
-
-  Router[method] = (path, callback) => {
-    //  console.log(Router +  " before")
-    console.log(Router)
-    Router.routes[method][path] = callback;
-    //  console.log(Router + " after")
-  }
-}
-*/
 
 Router.handle = (req, res) => {
   let method = req.method.toLowerCase();
   let path = url.parse(req.url).pathname;
 
-
-  // returns the matches object from the parser. that object will validate that the route is correct and pass back a params object if route was parameterized.
-
     if (Router.routes[method][path]) {
       Router.routes[method][path](req, res);
-
     } else {
       res.statusCode = 404
       res.end("404 Not Found");
     }
   };
 
-  /*
-  if (Router.routes[method][path]) {
-
-    // Use a promise to always resolve
-    // but allow async post data extraction
-    var p = new Promise((resolve) => {
-      if (method !== 'get') {
-        parser._extractPostData(req, resolve);
-      } else {
-        resolve();
-      }
-    });
-    p.then(function() {
-      Router.routes[method][path](req, res);
-    });
-  } else {
-    res.statusCode = 404
-    res.end("404 Not Found");
-  }
-  */
 
 
 module.exports = Router
--- services/getCommitData.js
+++ services/getCommitData.js
@@ -24,8 +24,9 @@ loadPages.index = (req, res) => {
   })
 }
 
+//this is the function to get and display info if the form is used.
 loadPages.form = (req, res) => {
-  let info = {}
+  let formInfo = {}
   var path = url.parse(req.url)
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
@@ -37,24 +38,19 @@ loadPages.form = (req, res) => {
       "Content-Type": "text/html"
     })
     var p = new Promise((resolve) => {
-      //console.log(path.href)
-      info.userNameMatch = path.href.match(/username=([^&]+)/)[1]
-      info.repoMatch = path.href.match(/repo=([a-zA-Z_-]+)/)[1]
-      //console.log(info.userNameMatch)
-      //console.log(info.repoMatch)
-      //info.userNameMatch = path.match(/username=([^&]+)/)[1]
-      //info.repoMatch = path.match(/repo=([a-zA-Z_]+)/)[1]
-      if (info.userNameMatch === null) {
+      formInfo.userNameMatch = path.href.match(/username=([^&]+)/)[1]
+      formInfo.repoMatch = path.href.match(/repo=([a-zA-Z_-]+)/)[1]
+      if (formInfo.userNameMatch === null) {
         return
       }
-      resolve(info)
-    }).then((info) => {
-      var results = github_request.getCommits(info)
-      return results
-    }).then((results) => {
+      resolve(formInfo)
+    }).then((formInfo) => {
+      let gitResults = github_request.getCommits(formInfo)
+      return gitResults
+    }).then((gitResults) => {
       var scrubCommits = []
       var someFuckingName = {}
-      for (var commit of results.data) {
+      for (var commit of gitResults.data) {
         scrubCommits.push(someFuckingName = {
           author: commit.commit.author,
           message: commit.commit.message,
@@ -63,28 +59,18 @@ loadPages.form = (req, res) => {
         })
         //scrubCommits = JSON.stringify(scrubCommits, null, 2)
         //fs.appendFileSync('./data/commits.json', JSON.stringify(someFuckingName, null,2))
-
       }
       scrubCommits = JSON.stringify(scrubCommits, null, 2)
-      //  fs.appendFileSync('./data/commits.json', scrubCommits, 'utf8')//, (err)=>{
-      //if(err) throw err;
-      //})
       //var htmlOut = data.replace("{{ commitFeed }}", JSON.stringify(scrubCommits, null, 2))
-
-
       var htmlOut = data.replace("{{ commitFeed }}", scrubCommits)
-      //console.log(commits)
-      //console.log()
       res.end(htmlOut)
-
-
     })
   })
 }
 
 
 loadPages.webHooks = (req, res) => {
-  let info = {}
+  let userAndRepoInfo = {}
   var path = url.parse(req.url)
   var _headers = {
     "Content-Type": "text/html",
@@ -101,24 +87,24 @@ loadPages.webHooks = (req, res) => {
     }
     res.writeHead(200, _headers)
     var webhookData = parser._extractPostData(req)
-
-    webhookData.then((payload) =>{
-
-        info.userNameMatch = payload.pusher.name
-        info.repoMatch = payload.repository.name
-
-      return info
-    }).then((info) => {
-      var results = github_request.getCommits(info)
-      return results
-    }).then((results) => {
-      results = JSON.stringify(results, null, 2)
-      fs.writeFileSync("./data/commits.json", results , 'utf8')
-      return results
-    }).then((results) => {
-
+    webhookData.then((payload) => {
+      userAndRepoInfo.userNameMatch = payload.pusher.name
+      userAndRepoInfo.repoMatch = payload.repository.name
+      return userAndRepoInfo
+    }).then((userAndRepoInfo) => {
+      let gitResults = github_request.getCommits(userAndRepoInfo)
+      return gitResults
+    }).then((gitResults) => {
+      gitResults = JSON.stringify(gitResults, null, 2)
+      fs.writeFileSync("./data/commits.json",gitResults, 'utf8')
+      return gitResults
+    }).then((gitResults) => {
       //var htmlOut = data.replace("{{ commitFeed }}", JSON.stringify(commits, null, 2))
       var htmlOut = data.replace("{{ commitFeed }}", results)
+      console.log(gitResults)
+      res.end(htmlOut)
+      //res.end('200 ok')
+    }).then((htmlOut) => {
       res.end(htmlOut)
     })
   })
